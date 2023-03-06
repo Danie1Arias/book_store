b795d5b47064d00b74bcf5ff75544833aa492094
Daniel Arias <darias1603@gmail.com>
2023-03-06 08:56:58 +0100

    Configure Administration

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -15,14 +15,16 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True)   # harry-potter-1
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True)   # harry-potter-1
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
 
+    """ #We control the slug field in the administration form (see admin.py)
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)     #Transforms the title tag into a slug tag format
         super().save(*args, **kwargs)
+    """
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
